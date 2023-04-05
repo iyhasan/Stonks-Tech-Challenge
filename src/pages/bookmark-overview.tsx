f2babe00cd26c203ff1de618f3a938ad7464efff
Ishaq Yousef Haj Hasan <ishaq.y.hajhasan@gmail.com>
2023-04-05 11:59:55 -0700

    use subscribe to update bookmark-overview when store is updated

--- src/pages/bookmark-overview.tsx
+++ src/pages/bookmark-overview.tsx
@@ -3,19 +3,36 @@ import MovieList from "@/components/movie-list";
 import { bookmarkStore } from "@/lib/store"
 import { BookmarkedMovie } from "@/types"
 import { Movie } from "@/types/rapidapi-movies";
+import { Box } from '@chakra-ui/react';
 
 function BookmarkOverview() {
 
-    const movieList = bookmarkStore((state) => state.movies);
+    const [movieList, setMovieList] = useState<BookmarkedMovie[]>([]);
+
+    useEffect(() => {
+    // Get movieList from the store on the client-side
+    const clientMovieList = bookmarkStore.getState().movies;
+    setMovieList(clientMovieList);
+
+    // Subscribe to store updates
+    const unsubscribe = bookmarkStore.subscribe((store) => {
+      setMovieList(store.movies);
+    });
+
+    // Clean up the subscription when the component is unmounted
+    return () => {
+      unsubscribe();
+    };
+  }, []);
 
     return (
-        <div>
+        <Box>
             {
                 movieList.map((mov: BookmarkedMovie) => (
-                    <p>{mov.movie.Title}</p>
+                    <Box>{mov.movie.Title}</Box>
                 ))
             }
-        </div>
+        </Box>
     )
 }
 
